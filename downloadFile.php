0c3a27fc6b032a22a0ba3287f6adb3f47e5ca9ed
fuliang <fuliang19890809@163.com>
2020-04-05 13:27:13 +0800

    提交

--- downloadFile.php
+++ downloadFile.php
@@ -1,20 +1,25 @@
 <?php
 // 文件下载，支持断点续传
-$params = $_GET;
-try {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $params = $_POST;
+    try {
 
-    if (empty($params['file_url'])) throw new \Exception('请传入文件地址！');
-    $fileUrl = $params['file_url'];
-    $fileName = $params['file_name'] ?? null;
-    $filePath = $params['file_path'] ?? null;
-    $chunkSize = $params['chunk_size'] ?? null;
+        if (empty($params['file_url'])) throw new \Exception('请传入文件地址！');
+        $fileUrl = trim($params['file_url']);
+        $fileName = $params['file_name'] ? trim($params['file_name']) : null;
+        $filePath = $params['file_path'] ? trim(str_replace('\\', '/', $params['file_path'])) : null;
+        $chunkSize = $params['chunk_size'] ? trim($params['chunk_size']) : null;
 
-    downloadFileHttp($fileUrl, $fileName, $filePath, $chunkSize);
+        $res = downloadFileHttp($fileUrl, $fileName, $filePath, $chunkSize);
+        if ($res['success'] == false) throw new \Exception($res['message']);
 
-} catch (\Exception $ex) {
+        echo $res['message'] . '<br/>文件大小：' . sprintf("%.2f", ($res['data']['file_size'] / 1024)) . 'KB<br/>文件路径：' . $res['data']['file_path'] . $res['data']['file_name'];
 
-    echo $ex->getMessage();
-}
+    } catch (\Exception $ex) {
+
+        echo $ex->getMessage();
+    }
+};
 
 /**
  * 远程文件下载，支持断点续传
@@ -22,31 +27,31 @@ try {
  * @param string|null $fileName 下载完成后的文件名 （可选参数：默认源文件名）
  * @param string|null $filePath 下载完成后的文件路径 （可选参数：默认当前文件目录下）
  * @param int|null $chunkSize 每次写入的大小,单位byte （可选参数：默认1M）
+ * @return array
+ *    [
+ *      "success":true,
+ *      "data":[
+ *          "file_size":23149,
+ *          "file_name":"77aba2f582515fadfb346241700bc843_thumb.jpg",
+ *          "file_path":"C:/Users/fulia/Desktop"
+ *      ],
+ *      "message":"下载成功！"
+ * ]
  * @example
  * $url = "http://ceshi-admin.sharcom.cn//uploads/member_files/order/2017/12/08/2cf4d497f9bd061b49d5c57857e11333.pdf";
  * $fileName = "1514518158.pdf";
  * $filePath = "uploads/member_files/order/20171229/";
  * $chunkSize = "1024 * 2048";
- * @return json
- * 	{
- *      "success":true,
- *      "data":{
- *          "file_size":23149,
- *          "file_name":"77aba2f582515fadfb346241700bc843_thumb.jpg",
- *          "file_path":"C:/Users/fulia/Desktop"
- *      },
- *      "message":"下载成功！"
- * }
  * @author fuliang
  * @date 2017-12-29
  */
-function downloadFileHttp (string $url, ?string $fileName = null, ?string $filePath = null, ?int $chunkSize = null)
+function downloadFileHttp(string $url, ?string $fileName = null, ?string $filePath = null, ?int $chunkSize = null): array
 {
     header('Content-Type: text/html; charset=utf-8');
-    $response = ['success' => false , 'data' => [], 'message' => '下载失败！'];
+    $response = ['success' => false, 'data' => [], 'message' => '下载失败！'];
     // 默认下载到当前目录下,如果指定了目录则下载到指定目录下,指定目录不存在则创建
     if (!empty($filePath)) {
-        if(!is_dir($filePath)) {
+        if (!is_dir($filePath)) {
             mkdir($filePath, 0755, true);
         }
     } else {
@@ -57,7 +62,12 @@ function downloadFileHttp (string $url, ?string $fileName = null, ?string $fileP
         if (empty($url)) {
             throw new \Exception('请传入文件地址！');
         }
-        if (is_file($filePath.$fileName)) {
+
+        if (substr($filePath, -1) != '/') {
+            $filePath .= '/';
+        }
+
+        if (is_file($filePath . $fileName)) {
             throw new \Exception('当前文件已存在！');
         }
         if (!empty($chunkSize)) {
@@ -77,14 +87,14 @@ function downloadFileHttp (string $url, ?string $fileName = null, ?string $fileP
         // http_range表示请求一个实体/文件的一个部分,用这个实现多线程下载和断点续传！
         if (isset($_SERVER['HTTP_RANGE'])) {
             header('HTTP /1.1 206 Partial Content');
-            $range = str_replace('=','-',$_SERVER['HTTP_RANGE']);
-            $range = explode('-',$range);
+            $range = str_replace('=', '-', $_SERVER['HTTP_RANGE']);
+            $range = explode('-', $range);
             $range = trim($range[1]);
-            header('Content-Length:'.$size);
-            header('Content-Range: bytes '.$range.'-'.$size2.'/'.$size);
+            header('Content-Length:' . $size);
+            header('Content-Range: bytes ' . $range . '-' . $size2 . '/' . $size);
         } else {
-            header('Content-Length:'.$size);
-            header('Content-Range: bytes 0-'.$size2.'/'.$size);
+            header('Content-Length:' . $size);
+            header('Content-Range: bytes 0-' . $size2 . '/' . $size);
         }
         header('Content-Description: File Transfer');
         header('Pragma: public');
@@ -93,12 +103,12 @@ function downloadFileHttp (string $url, ?string $fileName = null, ?string $fileP
         if ($fp == false) {
             throw new \Exception('文件不存在或打开失败！');
         }
-        @fseek($fp,$range);
+        @fseek($fp, $range);
         ob_clean();
         ob_end_flush();
         set_time_limit(0);
         // 写入方式打开文件，将文件指针指向文件末尾。如果文件不存在则尝试创建之
-        $fp2 = fopen($filePath.$fileName, 'a');
+        $fp2 = fopen($filePath . $fileName, 'a');
         while (!feof($fp)) {
             $buffer = fread($fp, $chunkSize);
             fwrite($fp2, $buffer); // 写入数据
@@ -108,7 +118,7 @@ function downloadFileHttp (string $url, ?string $fileName = null, ?string $fileP
         fclose($fp);
         fclose($fp2);
         // 下载完成后的文件大小,单位byte
-        $fileSize = filesize($filePath.$fileName);
+        $fileSize = filesize($filePath . $fileName);
         $response = [
             'success' => true,
             'data' => [
@@ -122,5 +132,11 @@ function downloadFileHttp (string $url, ?string $fileName = null, ?string $fileP
         $response['message'] = $e->getMessage();
     }
 
-    echo json_encode($response);
+    return $response;
 }
+?>
+<form method="post">
+    文件地址：<input type="text" name="file_url"/><br/>
+    保存地址：<input type="text" name="file_path"/><br/>
+    <input type="submit" value="下载"/>
+</form>
