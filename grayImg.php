1ea8ffa63ed240a8acd5a274a7e890695fb16298
fuliang <fuliang19890809@163.com>
2020-04-05 17:46:22 +0800

    提交

--- grayImg.php
+++ grayImg.php
@@ -2,29 +2,25 @@
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     require_once './uploadFile.php';
     $file = $_FILES['file'];
+    file_put_contents("./debug.txt", date('Y/m/d H:i:s', time()) . " \t输出结果:" . var_export($file, true) . "\r\n\r\n", FILE_APPEND);
     $response = ['error' => 0, 'message' => '操作成功'];
     try {
 
-        if ($file['error'] > 0) {
-            throw new \Exception('上传失败');
-        }
-        if (!is_uploaded_file($file['tmp_name'])) {
-            throw new \Exception('无效得上传图片');
-        }
-        if ($file['size'] > (1024 * 1024 * 5)) {
-            throw new \Exception('上传图片不能大于5MB');
-        }
         if (!in_array($file['type'], ["image/jpeg", "image/png"])) {
             throw new \Exception('只允许上传jpg、jpeg、png格式的图片');
         }
 
         // 保存图片
         $result = uploadFile($file);
+        file_put_contents("./debug.txt", date('Y/m/d H:i:s', time()) . " \t输出结果:" . var_export($result, true) . "\r\n\r\n", FILE_APPEND);
+
         if ($result['success'] == false) {
             throw new \Exception($result['message']);
         }
 
         $grayPath = grayImg($result['file_info']['file_path'], $result['file_info']['file_type']);
+        file_put_contents("./debug.txt", date('Y/m/d H:i:s', time()) . " \t输出结果:" . var_export($grayPath, true) . "\r\n\r\n", FILE_APPEND);
+
         if (!$grayPath) throw new \Exception('生成图片失败');
 
         $response['file_path'] = 'http://yimuyuan.xin/tool/' . $grayPath;
